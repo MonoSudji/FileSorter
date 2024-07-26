c979c7a8056aec257c5bd7a7d40cc990b64f24c7
Mono <misupriority@gmail.com>
2024-07-26 12:01:39 +0500

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module kz.append.sortapplication {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens kz.append.sortapplication to javafx.fxml;
+    exports kz.append;
+    opens kz.append to javafx.fxml;
+}
